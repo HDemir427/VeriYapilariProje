825441547631310f4fdfc10e074bdc467cf8e8aa
HDemir427 <hazardemir1456@gmail.com>
2025-05-06 17:28:57 +0300

    Update ProductsController.cs

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,6 +1,45 @@
-﻿namespace OrderManagementSystem.Controllers
+using Microsoft.AspNetCore.Mvc;
+using OrderManagementSystem.Data.Entity;
+
+namespace OrderManagementSystem.Controllers
 {
-    public class Class
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
     {
+        private readonly IProductService _service;
+
+        public ProductsController(IProductService service) => _service = service;
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            try
+            {
+                _service.AddProduct(product);
+                return Created($"/products/{product.Id}", product);
+            }
+            catch (ArgumentException ex)
+            {
+                return Conflict(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(string id)
+        {
+            try
+            {
+                return Ok(_service.GetProduct(id));
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+
     }
 }
+
+
