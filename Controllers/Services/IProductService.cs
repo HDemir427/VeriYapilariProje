acfed492739a716368d3aac9cdbe4a42d4131ed5
HDemir427 <hazardemir1456@gmail.com>
2025-05-09 18:07:22 +0300

    Add files via upload

---
+++ Controllers/Services/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using OrderManagementSystem.Data.Entity;
+
+namespace OrderManagementSystem.Services;
+
+public interface IProductService
+{
+    void AddProduct(Product product);
+    Product GetProduct(string id);
+    IEnumerable<Product> Search(string keyword);
+    List<Product> GetByCategory(string category);
+    void ProcessOrder(Order order);
+}
