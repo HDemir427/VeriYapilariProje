d5215f0ca40697612ed39cea0505e25b56b347c0
HDemir427 <hazardemir1456@gmail.com>
2025-05-09 18:09:48 +0300

    Add files via upload

---
+++ Utilities/CustomQueue.cs
@@ -0,0 +1,38 @@
+﻿namespace OrderManagementSystem.Utilities;
+
+public class CustomQueue<T>
+{
+    private readonly LinkedList<T> _list = new LinkedList<T>();
+    private readonly object _lock = new object();
+
+    public void Enqueue(T item)
+    {
+        lock (_lock)
+        {
+            _list.AddLast(item);
+        }
+    }
+
+    public T Dequeue()
+    {
+        lock (_lock)
+        {
+            if (_list.Count == 0) throw new InvalidOperationException("Queue is empty");
+
+            var value = _list.First.Value;
+            _list.RemoveFirst();
+            return value;
+        }
+    }
+
+    public int Count
+    {
+        get
+        {
+            lock (_lock)
+            {
+                return _list.Count;
+            }
+        }
+    }
+}
