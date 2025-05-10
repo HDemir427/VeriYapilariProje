3a99338c6a9fbdbe323d562a2a83c7915d31c57c
HDemir427 <hazardemir1456@gmail.com>
2025-05-10 13:38:38 +0300

    Update ProductService.cs

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -2,15 +2,15 @@
 using OrderManagementSystem.Utilities;
 
 namespace OrderManagementSystem.Services;
-
 public class ProductService : IProductService
 {
-    private readonly CustomHashTable<string, Product> _products;
+    private readonly CustomHashTable<int, Product> _products; 
     private readonly CustomAvlTree<Category> _categories;
     private readonly CustomQueue<Order> _orders;
+    private readonly object _syncLock = new object();
 
     public ProductService(
-        CustomHashTable<string, Product> products,
+        CustomHashTable<int, Product> products,
         CustomAvlTree<Category> categories,
         CustomQueue<Order> orders)
     {
@@ -21,25 +21,34 @@ public class ProductService : IProductService
 
     public void AddProduct(Product product)
     {
-        _products.Add(product.ProductId.ToString(), product); // buraya Tostring ekledim
-        var category =  product.Category ; // buradaki new Categoryi kaldırdım şu an zaten bir class döndürüyor
-        var existingCategory = _categories.Find(category);
-        if (existingCategory != null)
-            existingCategory.Products.Add(product);
-        else
+        lock (_syncLock)
         {
-            category.Products.Add(product);
-            _categories.Insert(category);
+            if (product.Category == null)
+                throw new ArgumentNullException(nameof(product.Category));
+
+            _products.Add(product.ProductId, product);
+
+            var existingCategory = _categories.Find(product.Category);
+            if (existingCategory != null)
+                existingCategory.Products.Add(product);
+            else
+            {
+                product.Category.Products.Add(product);
+                _categories.Insert(product.Category);
+            }
         }
     }
 
-    public Product GetProduct(string id) => _products.Get(id);
+    public Product GetProduct(int id) => _products.Get(id); 
 
     public IEnumerable<Product> Search(string keyword) =>
         _products.GetAll().Where(p => p.Name.Contains(keyword));
 
-    public List<Product> GetByCategory(string category) =>
-        _categories.SearchCategory(category);
+    public List<Product> GetByCategory(string category)
+    {
+        var categoryNode = _categories.Find(new Category { Name = category });
+        return categoryNode?.Products ?? new List<Product>();
+    }
 
     public void ProcessOrder(Order order) => _orders.Enqueue(order);
 }
