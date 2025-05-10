460a8078b51226579d7c25e59ed3110ad303bac6
HDemir427 <hazardemir1456@gmail.com>
2025-05-10 14:09:03 +0300

    Add files via upload

---
+++ Services/UserHistory.cs
@@ -0,0 +1,27 @@
+﻿
+using OrderManagementSystem.Data.Entity;
+using OrderManagementSystem.Utilities;
+
+    namespace OrderManagementSystem.Services
+    {
+        public class UserHistoryService : IUserHistoryService
+        {
+            private readonly CustomLinkedList<UserHistory> _userHistories;
+
+            public UserHistoryService(CustomLinkedList<UserHistory> userHistories)
+            {
+                _userHistories = userHistories;
+            }
+
+            public void AddHistory(UserHistory history)
+            {
+                _userHistories.AddLast(history);
+            }
+
+            public List<UserHistory> GetUserHistory(int userId)
+            {
+                return _userHistories.Where(h => h.UserId == userId).ToList();
+            }
+        }
+    }
+
