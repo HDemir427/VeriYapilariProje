e93742499f398cafdffae49c43adb21fef93ee10
HDemir427 <hazardemir1456@gmail.com>
2025-05-10 14:08:04 +0300

    Add files via upload

---
+++ Utilities/CustomLinkedList.cs
@@ -0,0 +1,28 @@
+﻿
+    using System.Collections.Generic;
+
+    namespace OrderManagementSystem.Utilities
+    {
+        public class CustomLinkedList<T>
+        {
+            private LinkedList<T> _list = new LinkedList<T>();
+            private readonly object _lock = new object();
+
+            public void AddLast(T item)
+            {
+                lock (_lock)
+                {
+                    _list.AddLast(item);
+                }
+            }
+
+            public IEnumerable<T> Where(Func<T, bool> predicate)
+            {
+                lock (_lock)
+                {
+                    return _list.Where(predicate);
+                }
+            }
+        }
+    }
+
