b5ac294d03df0fe6382ec06cbd37a74c3da73dbc
HDemir427 <hazardemir1456@gmail.com>
2025-05-11 12:44:40 +0300

    Update AuthService.cs

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using OrderManagementSystem.Data.Entity;
-using OrderManagementSystem.Data.Dto;
+using OrderManagementSystem.Dto;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
